92a32568d7864bdd5f7acdef2d2e7c30ba7dccf6
dongyo <dkang@mz.co.kr>
2019-06-05 14:19:49 +0900

    Merge branch 'master' of https://git.pyengine.net/cloudone/wconsole-server into CLOUD-118
    # Conflicts:
    #	package.json
    #	src/app.js
    #	src/config/config.js

--- src/app.js
+++ src/app.js
@@ -11,10 +11,10 @@ import cors from 'cors';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
 import session from 'express-session';
-import {v5 as uuidv5} from 'uuid';
+
 import redis from 'redis';
 import connectRedis from 'connect-redis';
-
+import authMiddleWare from '@/auth/authMiddleWare';
 
 /* =======================
     LOAD THE CONFIG
@@ -29,8 +29,6 @@ app.set('view engine', 'ejs');
 app.set('views', __dirname+ '/views');
 
 app.use(logger('dev'));
-
-app.use(morgan('dev'));
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -38,9 +36,9 @@ app.use(cookieParser());
 app.use(cors());
 
 
-app.use('/api-docs', config.swagger.serve, config.swagger.setup);
+app.use('/api-docs', config.swagger('serve'), config.swagger('setup'));
 
-app.use('/api/', indexRouter);
+
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
   next(createError(404));
@@ -72,18 +70,18 @@ const sess = {
     logErrors: true }),
 }
 
-
 app.use(session(sess));
 app.use('/api/', indexRouter);
 
-// error
-app.use((err, req, res) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// // error
+// app.use((err, req, res) => {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// })
 
 app.use((req, res, next) => {
     console.log('****resposne:****', res);
@@ -100,4 +98,5 @@ config.connectListener(app);
 ========================== */
 config.expressConnect(mongoose);
 // testCommon.runUnittest(express);
+
 export default app;
