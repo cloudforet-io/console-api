ad2d1a030c6f6e51b12dca6ed7ecfce5b7ee7375
dongyo <dkang@mz.co.kr>
2019-06-07 01:37:56 +0900

    summarize all whitelist for cors : CLOUD-118

--- src/config/config.js
+++ src/config/config.js
@@ -4,7 +4,9 @@ import redis from 'redis';
 
 export default {
   secretKey: 'thisIsCloudOneSecretControlKey',
-  redisClient: redis.createClient({port: process.env.REDIS_PORT, host: process.env.REDIS_HOST, password: process.env.REDIS_PASSWORD}),
+  redisClient: redis.createClient({ port: process.env.REDIS_PORT, host: process.env.REDIS_HOST, password: process.env.REDIS_PASSWORD }),
+  setRedisSession: {
+  },
   setCurrrentEnv(environments) {
     /*
      * TODO Tenmp environment is only temporarily; Please remove this when all environment is settled.
@@ -28,32 +30,36 @@ export default {
   connectListener(app) {
     if (process.env.NODE_ENV === 'temp' || process.env.NODE_ENV === 'local') app.listen(process.env.APP_PORT, () => { console.log(`Server is listening on port: ${process.env.APP_PORT}`); });
   },
-  sessionNamer(v) {
-    const uuIdRandom = v();
-    return uuIdRandom;
+  expressConnect(database) {
+    const db = database.connection;
+    let mongodb = 'mongodb://';
+    db.on('error', console.error);
+    db.once('open', () => {
+      const date = new Date();
+      console.log(`Connection to mongo database has established at : ${date.toLocaleString()}`);
+    });
+    const dbEnv = [':', process.env.DB_USER, ':', process.env.DB_PASS, '@', process.env.DB_HOST, ':', process.env.DB_PORT, '/', process.env.DB_SERVICE];
+    // eslint-disable-next-line eqeqeq
+    console.log('Current Envrionment: ', process.env.NODE_ENV);
+    dbEnv.forEach((e, i) => { if (e && i != 0 && dbEnv[i - 1]) mongodb += e; });
+    if (process.env.NODE_ENV != 'prod') console.log(`Database Connected to: ${mongodb}`);
+    database.connect(mongodb, { useNewUrlParser: true });
   },
-  connectRedis(command, key, value) {
-    if (command === 'connect') {
-      this.redisClient.on('command', () => {
-        console.log('Redis client connected');
-      });
-    } else if (command === 'error') {
-      this.redisClient.on('command', () => {
-        console.log('Redis client connected');
-      });
-    } else if (command === 'set') {
-      this.redisClient.set(key, value, redis.print);
-    } else if (command === 'get') {
-      this.redisClient.get(key, (error, result) => {
-        if (error) {
-          console.log(error);
-          throw error;
+  corrOptionPreperation(whitelist, credential) {
+    const consList = (whitelist.indexOf(',') !== -1) ? whitelist.split(',') : whitelist;
+    console.log('Cons lists allowed => ', consList);
+    const corsOptions = {
+      credentials: credential,
+      origin: (origin, callback) => {
+        console.log('origin from => ', origin);
+        if (consList.indexOf(origin) !== -1) {
+          callback(null, true);
+        } else {
+          callback(new Error(`Not allowed by CORS with Requested URL: ${origin}`));
         }
-        console.log(`key: ${key} => ${result}`);
-      });
-    } else {
-      console.log('Unexpected indent Occurred');
-    }
+      },
+    };
+    return corsOptions;
   },
   printImportedmodule(fileSystem) {
     fileSystem.readdir('./node_modules', (err, dirs) => {
@@ -78,21 +84,6 @@ export default {
       });
     });
   },
-  expressConnect(database) {
-    const db = database.connection;
-    let mongodb = 'mongodb://';
-    db.on('error', console.error);
-    db.once('open', () => {
-      const date = new Date();
-      console.log(`Connection to mongo database has established at : ${date.toLocaleString()}`);
-    });
-    const dbEnv = [':', process.env.DB_USER, ':', process.env.DB_PASS, '@', process.env.DB_HOST, ':', process.env.DB_PORT, '/', process.env.DB_SERVICE];
-    // eslint-disable-next-line eqeqeq
-    console.log('Current Envrionment: ', process.env.NODE_ENV);
-    dbEnv.forEach((e, i) => { if (e && i != 0 && dbEnv[i - 1]) mongodb += e; });
-    if (process.env.NODE_ENV != 'prod') console.log(`Database Connected to: ${mongodb}`);
-    database.connect(mongodb, { useNewUrlParser: true });
-  },
   swagger(type) {
     const swaggerDefinition = {
       info: {
@@ -101,30 +92,14 @@ export default {
         description: 'CloudOne API',
       },
     };
-
     const options = {
       swaggerDefinition,
-      apis: ['/routes*.js', './example/parameters.yaml']
+      apis: ['/routes*.js', './example/parameters.yaml'],
     };
-
-    if(type === 'serve'){
-      return swaggerUi.serve
-    }else if(type === 'setup'){
-      return swaggerUi.setup(swaggerJSDoc(options))
+    if (type === 'serve') {
+      return swaggerUi.serve;
+    } if (type === 'setup') {
+      return swaggerUi.setup(swaggerJSDoc(options));
     }
-  },
-  corrOptionPreperation(whitelist, credential) {
-    const corsOptions = {
-      credentials: credential,
-      origin: (origin, callback) => {
-        console.log('origin from => ' , origin);
-        if (whitelist.indexOf(origin) !== -1) {
-          callback(null, true);
-        } else {
-          callback(new Error('Not allowed by CORS with Requested URL: ' + origin));
-        }
-      },
-    };
-    return corsOptions;
   },
 };
