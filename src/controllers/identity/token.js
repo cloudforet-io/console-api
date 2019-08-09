f68a54a8952647dd0a8eb16e666830db2f2f1c10
whdalsrnt <whdalsrnt@mz.co.kr>
2019-08-09 16:49:01 +0900

    Remove Redis

--- src/controllers/identity/token.js
+++ src/controllers/identity/token.js
@@ -1,15 +1,15 @@
 import grpcClient from '@lib/grpc-client';
 import * as wellKnownType from '@lib/grpc-client/well-known-type';
-import redisClient from '@lib/redis';
-import redis from 'redis';
+// import redisClient from '@lib/redis';
+// import redis from 'redis';
 
 const issueToken = async (params) => {
     let identityV1 = await grpcClient.get('identity', 'v1');
     let response = await identityV1.Token.issue(params);
 
-    let client = await redisClient.get();
-    client.set(response.access_token, response.refresh_token, redis.print);
-    client.get(response.access_token);
+    // let client = await redisClient.get();
+    // client.set(response.access_token, response.refresh_token, redis.print);
+    // client.get(response.access_token);
     //console.log(client.get(response.access_token));
 
     delete response.refresh_token;
