7fb4395b338a8403ec77be53c25151aa29bb0de5
whdalsrnt <whdalsrnt@mz.co.kr>
2019-08-16 15:27:03 +0900

    Update source

--- src/controllers/identity/project/tree-project.js
+++ src/controllers/identity/project/tree-project.js
@@ -1,4 +1,102 @@
 import grpcClient from '@lib/grpc-client';
+import _ from 'lodash';
+
+const getProjectGroups = async (client, params) => {
+    let reqParams = {
+        _meta: params._meta,
+        query: params.query,
+        parent_project_group_id: params.item_id || null
+    };
+
+    let response = await client.ProjectGroup.list(reqParams);
+    let items = [];
+    response.results.map((itemInfo) => {
+        let item = {
+            id: itemInfo.project_group_id,
+            name: itemInfo.name,
+            has_child: true,
+            item_type: 'PROJECT_GROUP'
+        };
+        items.push(item);
+    });
+
+    return items;
+};
+
+const getProjects = async (client, params) => {
+    let reqParams = {
+        _meta: params._meta,
+        query: params.query,
+        project_group_id: params.item_id || null
+    };
+
+    let response = await client.Project.list(reqParams);
+    let items = [];
+    response.results.map((itemInfo) => {
+        let item = {
+            id: itemInfo.project_id,
+            name: itemInfo.name,
+            has_child: false,
+            item_type: 'PROJECT'
+        };
+        items.push(item);
+    });
+
+    return items;
+};
+
+const getParentItem = async (client, itemId, itemType, meta, openItems = []) => {
+    let reqParams = {
+        _meta: meta,
+        query: {
+            minimal: false
+        }
+    };
+
+    if (itemType == 'PROJECT') {
+        reqParams.project_id = itemId;
+        let response = await client.Project.list(reqParams);
+
+        if (response.total_count == 1) {
+            let projectInfo = response.results[0];
+            openItems.push(projectInfo.project_id);
+
+            let parentItemId = _.get(projectInfo, 'project_group_info.project_group_id');
+            if (parentItemId) {
+
+                await getParentItem(
+                    client,
+                    parentItemId,
+                    'PROJECT_GROUP',
+                    meta,
+                    openItems
+                );
+            }
+        }
+    } else {
+        reqParams.project_group_id = itemId;
+        let response = await client.ProjectGroup.list(reqParams);
+
+        if (response.total_count == 1) {
+            let projectGroupInfo = response.results[0];
+            openItems.push(projectGroupInfo.project_group_id);
+
+            let parentItemId = _.get(projectGroupInfo, 'parent_project_group_info.project_group_id');
+            if (parentItemId) {
+
+                await getParentItem(
+                    client,
+                    parentItemId,
+                    'PROJECT_GROUP',
+                    meta,
+                    openItems
+                );
+            }
+        }
+    }
+
+    return openItems;
+};
 
 const treeProject = async (params) => {
     if (!params.query) {
@@ -6,28 +104,28 @@ const treeProject = async (params) => {
     }
     //params.query.minimal = true;
 
+    if (params.sort) {
+        params.query.sort = params.sort;
+    }
+
     let identityV1 = await grpcClient.get('identity', 'v1');
-
-    // if (params.itemType == 'project_group') {
-    //     let response = await identityV1.ProjectGroup.list(params);
-    // } else if (params.itemType == 'project') {
-    //     let response = await identityV1.Project.list(params);
-    // } else {
-    //     throw new Error(`Parameter is invalid. (item_type = ${params.item_type})`);
-    // }
-    //
-    // response.results.map((itemInfo) => {
-    //     let item = {
-    //         id: (params.itemType == 'project')? itemInfo.project_id : itemInfo.project_group_id,
-    //         name: itemInfo.name,
-    //         hasChild: true,
-    //         itemType: itemInfo
-    //     };
-    // });
-
-    return {
+    let response = {
+        open_items: [],
         items: []
     };
+
+    if (params.search) {
+        response.open_items = await getParentItem(
+            identityV1,
+            params.search.item_id,
+            params.search.item_type,
+            params._meta);
+    }
+
+    response.items = _.merge(response.items, await getProjects(identityV1, params));
+    response.items = _.merge(response.items, await getProjectGroups(identityV1, params));
+
+    return response;
 };
 
 export default treeProject;
