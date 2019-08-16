fabe73a92929c8a8ceb372df66fd729757ce5e67
whdalsrnt <whdalsrnt@mz.co.kr>
2019-08-16 18:19:23 +0900

    Update source

---
+++ src/controllers/identity/policy/index.js
@@ -0,0 +1,44 @@
+import grpcClient from '@lib/grpc-client';
+
+const createPolicy = async (params) => {
+    let identityV1 = await grpcClient.get('identity', 'v1');
+    let response = await identityV1.Policy.create(params);
+
+    return response;
+};
+
+const updatePolicy = async (params) => {
+    let identityV1 = await grpcClient.get('identity', 'v1');
+    let response = await identityV1.Policy.update(params);
+
+    return response;
+};
+
+const deletePolicy = async (params) => {
+    let identityV1 = await grpcClient.get('identity', 'v1');
+    let response = await identityV1.Policy.delete(params);
+
+    return response;
+};
+
+const getPolicy = async (params) => {
+    let identityV1 = await grpcClient.get('identity', 'v1');
+    let response = await identityV1.Policy.get(params);
+
+    return response;
+};
+
+const listPolicys = async (params) => {
+    let identityV1 = await grpcClient.get('identity', 'v1');
+    let response = await identityV1.Policy.list(params);
+
+    return response;
+};
+
+export {
+    createPolicy,
+    updatePolicy,
+    deletePolicy,
+    getPolicy,
+    listPolicys
+};
