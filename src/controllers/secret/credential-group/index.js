48eeeabbcb0508ab42ad3670f9acb1b72aa990fc
whdalsrnt <whdalsrnt@mz.co.kr>
2020-01-31 14:21:32 +0900

    Bug Fix: change multiple job logic (promise map -> for)

--- src/controllers/secret/credential-group/index.js
+++ src/controllers/secret/credential-group/index.js
@@ -40,17 +40,83 @@ const listCredentialGroups = async (params) => {
 };
 
 const addCredential = async (params) => {
-    let secretV1 = await grpcClient.get('secret', 'v1');
-    let response = await secretV1.CredentialGroup.add_credential(params);
+    if (!params.credentials) {
+        throw new Error('Required Parameter. (key = credentials)');
+    }
 
-    return response;
+    let secretV1 = await grpcClient.get('secret', 'v1');
+
+    let successCount = 0;
+    let failCount = 0;
+    let failItems = {};
+
+    for (let i=0; i < params.credentials.length; i++) {
+        let credential_id = params.credentials[i];
+        try {
+            let reqParams = {
+                credential_id: credential_id,
+                credential_group_id: params.credential_group_id
+            };
+
+            if (params.domain_id) {
+                reqParams.domain_id = params.domain_id;
+            }
+
+            await secretV1.CredentialGroup.add_credential(reqParams);
+            successCount = successCount + 1;
+        } catch (e) {
+            failItems[credential_id] = e.details || e.message;
+            failCount = failCount + 1;
+        }
+    }
+
+    if (failCount > 0) {
+        let error = new Error(`Failed to add credentials. (success: ${successCount}, failure: ${failCount})`);
+        error.fail_items = failItems;
+        throw error;
+    } else {
+        return {};
+    }
 };
 
 const removeCredential = async (params) => {
-    let secretV1 = await grpcClient.get('secret', 'v1');
-    let response = await secretV1.CredentialGroup.remove_credential(params);
+    if (!params.credentials) {
+        throw new Error('Required Parameter. (key = credentials)');
+    }
 
-    return response;
+    let secretV1 = await grpcClient.get('secret', 'v1');
+
+    let successCount = 0;
+    let failCount = 0;
+    let failItems = {};
+
+    for (let i=0; i < params.credentials.length; i++) {
+        let credential_id = params.credentials[i];
+        try {
+            let reqParams = {
+                credential_id: credential_id,
+                credential_group_id: params.credential_group_id
+            };
+
+            if (params.domain_id) {
+                reqParams.domain_id = params.domain_id;
+            }
+
+            await secretV1.CredentialGroup.remove_credential(reqParams);
+            successCount = successCount + 1;
+        } catch (e) {
+            failItems[credential_id] = e.details || e.message;
+            failCount = failCount + 1;
+        }
+    }
+
+    if (failCount > 0) {
+        let error = new Error(`Failed to remove credentials. (success: ${successCount}, failure: ${failCount})`);
+        error.fail_items = failItems;
+        throw error;
+    } else {
+        return {};
+    }
 };
 
 export {
