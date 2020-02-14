39deaed77993e6b3e4ce78fb655489afd331ecdc
whdalsrnt <whdalsrnt@mz.co.kr>
2020-02-15 05:19:04 +0900

    Remove issue token api

--- src/routes/identity/index.js
+++ src/routes/identity/index.js
@@ -7,7 +7,6 @@ import policyRouter from './policy';
 import roleRouter from './role';
 import userRouter from './user';
 import apiKeyRouter from './api-key';
-import tokenRouter from './token';
 
 const router = express.Router();
 router.use('/domain', domainRouter);
@@ -18,6 +17,5 @@ router.use('/policy', policyRouter);
 router.use('/role', roleRouter);
 router.use('/user', userRouter);
 router.use('/api-key', apiKeyRouter);
-router.use('/token', tokenRouter);
 
 export default router;
