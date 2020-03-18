cc5b4d338e0d19c83d14230405236076987713a1
dongyo <dkang@mz.co.kr>
2020-03-18 23:37:08 +0900

    Added Plugins API

---
+++ src/controllers/plugin/supervisor/index.js
@@ -0,0 +1,74 @@
+import grpcClient from '@lib/grpc-client';
+import { changeQueryKeyword } from '@lib/utils';
+import logger from '@lib/logger';
+
+const getSupervisor = async (params) => {
+    const pluginV1 = await grpcClient.get('plugin', 'v1');
+    let response = await pluginV1.Supervisor.get(params);
+
+    return response;
+};
+
+const listSupervisor = async (params) => {
+    const pluginV1 = await grpcClient.get('plugin', 'v1');
+    let response = await pluginV1.Supervisor.list(params);
+    return response;
+};
+
+const recoverPlugin = async (params) => {
+    if (!params.supervisor_id) {
+        throw new Error('Required Parameter. (key = supervisor_id)');
+    } else if(!params.plugins){
+        throw new Error('Required Parameter. (key = plugins)');
+    }
+
+    const pluginV1 = await grpcClient.get('plugin', 'v1');
+
+    let successCount = 0;
+    let failCount = 0;
+    let failItems = {};
+
+    for (let i=0; i < params.plugins.length; i++) {
+        const plugin = params.plugins[i];
+        try {
+            let reqParams = {
+                supervisor_id: params.supervisor_id,
+                plugin_id: plugin.plugin_id,
+                version: plugin.version
+            };
+
+            if (params.domain_id) {
+                reqParams.domain_id = params.domain_id;
+            }
+            console.log('reqParams: ', reqParams);
+            await pluginV1.Supervisor.recover_plugin(reqParams);
+            successCount = successCount + 1;
+        } catch (e) {
+            const plugin = params.plugins[i];
+            failItems[plugin.plugin_id] = e.details || e.message;
+            failCount = failCount + 1;
+        }
+    }
+
+    if (failCount > 0) {
+        let error = new Error(`Failed to recover plugins. (success: ${successCount}, failure: ${failCount})`);
+        error.fail_items = failItems;
+        throw error;
+    } else {
+        return {};
+    }
+};
+
+const listPlugin = async (params) => {
+    const pluginV1 = await grpcClient.get('plugin', 'v1');
+    let response = await pluginV1.Supervisor.list_plugins(params);
+    return response;
+};
+
+
+export {
+    recoverPlugin,
+    listPlugin,
+    getSupervisor,
+    listSupervisor
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ import repositoryRouter from './repository';
 import secretRouter from './secret';
 import inventoryRouter from './inventory';
 import addOnsRouter from './add-ons';
+import pluginRouter from './plugin';
 
 const router = express.Router();
 router.use('/statistics', statisticsRouter);
@@ -13,4 +14,5 @@ router.use('/repository', repositoryRouter);
 router.use('/secret', secretRouter);
 router.use('/inventory', inventoryRouter);
 router.use('/add-ons', addOnsRouter);
+router.use('/plugin', pluginRouter);
 export default router;
