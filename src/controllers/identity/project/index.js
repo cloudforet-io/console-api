d22f2ccf2174ddd5c2d649644e3acb1af20ab341
dongyo <dkang@mz.co.kr>
2020-04-01 19:35:08 +0900

    added project/get
    availability
    param = { include_provider, true }

--- src/controllers/identity/project/index.js
+++ src/controllers/identity/project/index.js
@@ -1,5 +1,6 @@
 import grpcClient from '@lib/grpc-client';
 import { changeQueryKeyword } from '@lib/utils';
+import _ from 'lodash';
 import logger from '@lib/logger';
 
 const createProject = async (params) => {
@@ -24,8 +25,40 @@ const deleteProject = async (params) => {
 };
 
 const getProject = async (params) => {
+
     let identityV1 = await grpcClient.get('identity', 'v1');
     let response = await identityV1.Project.get(params);
+
+    if(params.include_provider) {
+        //const providers = [];
+        const service_params = {
+            project_id: params.project_id,
+            domain_id:params.domain_id
+        };
+
+        const service_accounts = await identityV1.ServiceAccount.list(service_params);
+
+        if(service_accounts.total_count > 0){
+            const prep = _.uniqBy(service_accounts.results, 'provider').map(a => a.provider);
+            /*for (let i=0; i < prep.length; i++) {
+                const provider = prep[i];
+                try {
+                    const providerPram = { provider }
+                    if (providerPram.domain_id) {
+                        providerPram.domain_id = params.domain_id;
+                    }
+
+                    const providerOutput = await identityV1.Provider.get(providerPram);
+                    const providerName = _.get(providerOutput, 'name');
+                    providers.push(providerName);
+
+                } catch (e) {
+                    console.log(e);
+                }
+            }*/
+            response.providers = prep.length > 0 ? prep : [];
+        }
+    }
 
     return response;
 };
