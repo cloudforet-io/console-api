19c152d0693061337e4808f49b70c579bde6b1ae
dongyo <dkang@mz.co.kr>
2020-05-12 16:08:55 +0900

    added provider when get a list of project under given project group.

--- src/controllers/identity/project-group/index.js
+++ src/controllers/identity/project-group/index.js
@@ -1,5 +1,6 @@
 import grpcClient from '@lib/grpc-client';
 import logger from '@lib/logger';
+import _ from "lodash";
 
 const createProjectGroup = async (params) => {
     let identityV1 = await grpcClient.get('identity', 'v1');
@@ -136,6 +137,25 @@ const listProjectGroups = async (params) => {
 const listProjects = async (params) => {
     let identityV1 = await grpcClient.get('identity', 'v1');
     let response = await identityV1.ProjectGroup.list_projects(params);
+
+    if (params.include_provider && response.results.length > 0) {
+        for(let i =0 ; i < response.results.length; i++ ){
+            const currentProject = response.results[i];
+            const service_params = {
+                project_id: currentProject.project_id,
+                domain_id:params.domain_id
+            };
+            const service_accounts = await identityV1.ServiceAccount.list(service_params);
+            if (service_accounts.total_count > 0) {
+                const prep = _.uniqBy(service_accounts.results, 'provider').map(a => a.provider);
+                response.results[i].providers = prep.length > 0 ? prep : [];
+            } else {
+                response.results[i].providers = [];
+            }
+        }
+
+    }
+
     return response;
 };
 
