cdd1bf5479135b46941c26c768baed56e037a185
sinsky <sinsky@magazone.com>
2020-05-28 19:39:00 +0900

    init apollo server

--- src/app.ts
+++ src/app.ts
@@ -6,9 +6,31 @@ import httpContext from 'express-http-context';
 import { authentication, corsOptions } from '@lib/authentication';
 import { requestLogger, errorLogger} from '@lib/logger';
 import { notFoundErrorHandler, defaultErrorHandler} from '@lib/error';
+import { ApolloServer, gql } from 'apollo-server-express';
+
+
 import indexRouter from 'routes';
 const app = express();
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
 
+// Provide resolver functions for your schema fields
+const resolvers = {
+    Query: {
+        hello: () => 'Hello world!'
+    }
+};
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    playground:true
+});
+
+server.applyMiddleware({ app });
+console.log('path',server.graphqlPath);
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -24,5 +46,6 @@ app.use(notFoundErrorHandler());
 
 app.use(errorLogger());
 app.use(defaultErrorHandler());
+
 
 export default app;
