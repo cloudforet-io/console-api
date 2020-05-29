4558b4dd885f9cf23d5f65186b40c12aa5f54f44
WANJIN <wanjin@megazone.com>
2020-05-29 15:33:16 +0900

    [CLOUD-1781] Added v2 - custom controllers added

--- src/routes/v2/index.ts
+++ src/routes/v2/index.ts
@@ -5,6 +5,23 @@ import asyncHandler from 'express-async-handler';
 import {proxyClient} from '@controllers/proxy/client';
 import addOnsRouter from "../add-ons";
 
+import * as secret from "@controllers/secret/secret";
+import * as secretGroup from "@controllers/secret/secret-group";
+import * as project from "@controllers/identity/project";
+import {treePathSearchProject, treeProject} from "@controllers/identity/project/tree-project";
+import * as projectGroup from "@controllers/identity/project-group";
+import * as serviceAccount from "@controllers/identity/service-account";
+import listServiceAccountMembers from "@controllers/identity/service-account/list-service-account-members";
+import * as user from "@controllers/identity/user";
+import * as supervisor from "@controllers/plugin/supervisor";
+import * as server from "@controllers/inventory/server";
+import serverGetData from "@controllers/inventory/server/get-data";
+import listServerMembers from "@controllers/inventory/server/list-server-members";
+import * as cloudService from "@controllers/inventory/cloud-service";
+import cloudServiceGetData from "@controllers/inventory/cloud-service/get-data";
+import listCloudServiceMembers from "@controllers/inventory/cloud-service/list-cloud-service-members";
+import * as collector from "@controllers/inventory/collector";
+
 
 const resourceMap = {
     identity: {
@@ -76,6 +93,52 @@ const getClient = (paths:string[])=>{
 }
 const router = express.Router();
 router.use('/add-ons', addOnsRouter);
+
+const controllers: {url: string, func: any}[] = [
+    { url: '/secret/secret/create', func: secret.createSecret },
+    { url: '/secret/secret-group/secret/add', func: secretGroup.addSecret },
+    { url: '/secret/secret-group/secret/remove', func: secretGroup.removeSecret },
+
+    { url: '/plugin/supervisor/plugin/recover', func: supervisor.recoverPlugin },
+
+    { url: '/identity/project/delete', func: project.deleteProject },
+    { url: '/identity/project/get', func: project.getProject },
+    { url: '/identity/project/list', func: project.listProjects },
+    { url: '/identity/project/member/add', func: project.addProjectMember },
+    { url: '/identity/project/member/remove', func: project.removeProjectMember },
+    { url: '/identity/project/tree', func: treeProject },
+    { url: '/identity/project/tree/search', func: treePathSearchProject },
+    { url: '/identity/project-group/member/remove', func: projectGroup.removeProjectGroupMember },
+    { url: '/identity/project-group/member/add', func: projectGroup.addProjectGroupMember },
+    { url: '/identity/project-group/list-projects', func: projectGroup.listProjects },
+    { url: '/identity/service-account/change-project', func: serviceAccount.changeServiceAccountProject },
+    { url: '/identity/service-account/member/list', func: listServiceAccountMembers },
+    { url: '/identity/user/delete', func: user.deleteUsers },
+    { url: '/identity/user/enable', func: user.enableUsers },
+    { url: '/identity/user/disable', func: user.disableUsers },
+
+    { url: '/inventory/server/change-state', func: server.changeServerState },
+    { url: '/inventory/server/change-project', func: server.changeServerProject },
+    { url: '/inventory/server/change-pool', func: server.changeServerPool },
+    { url: '/inventory/server/delete', func: server.deleteServers },
+    { url: '/inventory/server/get-data', func: serverGetData },
+    { url: '/inventory/server/member/list', func: listServerMembers },
+
+    { url: '/inventory/cloud-service/change-region', func: cloudService.changeCloudServiceRegion },
+    { url: '/inventory/cloud-service/change-project', func: cloudService.changeCloudServiceProject },
+    { url: '/inventory/cloud-service/delete', func: cloudService.changeCloudServiceRegion },
+    { url: '/inventory/cloud-service/get-data', func: cloudServiceGetData },
+    { url: '/inventory/cloud-service/member/list', func: listCloudServiceMembers },
+
+    { url: '/inventory/collector/enable', func: collector.enableCollectors },
+    { url: '/inventory/collector/disable', func: collector.disableCollectors },
+    { url: '/inventory/collector/delete', func: collector.deleteCollectors },
+];
+controllers.forEach((config) => {
+    router.post(config.url, asyncHandler(async (req, res, next) => {
+        res.json(await config.func(req.body));
+    }));
+});
 router.post('/*', asyncHandler(proxyHandler(getClient)));
 
 export default router;
