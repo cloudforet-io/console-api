555d61c22534029c2b3d5dc735ab5d4681fe6a83
Jongmin Kim <whdalsrnt@megazone.com>
2020-08-02 23:02:26 +0900

    Add repository policy APIs
    Signed-off-by: Jongmin Kim <whdalsrnt@megazone.com>

--- src/routes/repository/index.js
+++ src/routes/repository/index.js
@@ -2,9 +2,11 @@ import express from 'express';
 import repositoryRouter from './repository';
 import pluginRouter from './plugin';
 import schemaRouter from './schema';
+import policyRouter from './policy';
 
 const router = express.Router();
 router.use('/repository', repositoryRouter);
 router.use('/plugin', pluginRouter);
 router.use('/schema', schemaRouter);
+router.use('/policy', policyRouter);
 export default router;
