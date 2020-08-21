5dadd9cdc882e7911bcf9d03e36235094e7d5482
Jongmin Kim <whdalsrnt@megazone.com>
2020-08-21 11:28:18 +0900

    Add start and end parameter to daily job summary Remove console log
    Signed-off-by: Jongmin Kim <whdalsrnt@megazone.com>

--- src/controllers/statistics/topic/daily-job-summary.js
+++ src/controllers/statistics/topic/daily-job-summary.js
@@ -40,12 +40,33 @@ const getDefaultQuery = () => {
 
 const makeRequest = (params) => {
     let requestParams = getDefaultQuery();
+
+    if (params.start && params.end) {
+        requestParams['query']['filter'] = [{
+            'k': 'created_at',
+            'v': params.start,
+            'o': 'datetime_gt'
+        }, {
+            'k': 'created_at',
+            'v': params.end,
+            'o': 'datetime_lt'
+        }];
+    } else {
+        requestParams['query']['filter'] = [{
+            'k': 'created_at',
+            'v': 'now/d-6d',
+            'o': 'timediff_gt'
+        }];
+    }
+
+
     return requestParams;
 };
 
 const dailyJobSummary = async (params) => {
     let statisticsV1 = await grpcClient.get('statistics', 'v1');
     const requestParams = makeRequest(params);
+    console.log(JSON.stringify(requestParams));
     let response = await statisticsV1.History.stat(requestParams);
 
     return response;
