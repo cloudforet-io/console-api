eb2617c3e368f66b86c8588b440a480c46d99249
Jongmin Kim <whdalsrnt@megazone.com>
2020-09-24 11:36:14 +0900

    Change mock data
    Signed-off-by: Jongmin Kim <whdalsrnt@megazone.com>

--- src/factories/power-scheduler/schedule.js
+++ src/factories/power-scheduler/schedule.js
@@ -5,7 +5,7 @@ class ScheduleResourceGroupFactory extends BaseFactory {
     constructor(fields = {}) {
         super();
         this.resource_group_id = `rs-${faker.random.uuid().substr(0,8)}`;
-        this.priority = faker.random.number({ min: 1, max: 5 });
+        this.priority = faker.random.number({ min: 1, max: 10 });
     }
 }
 
@@ -15,7 +15,7 @@ export class ScheduleFactory extends BaseFactory {
         this.schedule_id = fields.schedule_id || `schedule-${faker.random.uuid().substr(0,8)}`;
         this.name = fields.name || faker.random.word();
         this.state = fields.state || 'ENABLED';
-        const resourceGroupCount = faker.random.number(3);
+        const resourceGroupCount = faker.random.number({min: 3, max: 10});
         this.resource_groups = ScheduleResourceGroupFactory.buildBatch(resourceGroupCount);
         this.tags = fields.tags || {
             [faker.random.word()]: faker.random.word(),
