378a0fad0cbe337cc71eb45ba3a4cffd8910ec72
Jongmin Kim <whdalsrnt@megazone.com>
2020-09-28 18:31:33 +0900

    Add power scheduler apis
    Signed-off-by: Jongmin Kim <whdalsrnt@megazone.com>

--- src/routes/power-scheduler/index.js
+++ src/routes/power-scheduler/index.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import scheduleRouter from './schedule';
 import scheduleRuleRouter from './schedule-rule';
+import controllerRouter from './controller';
+import jobRouter from './job';
+import jobTaskRouter from './job-task';
 
 const router = express.Router();
 
 router.use('/schedule', scheduleRouter);
 router.use('/schedule-rule', scheduleRuleRouter);
+router.use('/controller', controllerRouter);
+router.use('/job', jobRouter);
+router.use('/job-task', jobTaskRouter);
 
 export default router;
