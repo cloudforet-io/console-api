b1492c0d01f1450483f7315c41dae973d7f9cb31
Jongmin Kim <whdalsrnt@megazone.com>
2020-10-19 20:33:02 +0900

    Add a recommended field to get-create-schedule-resource-groups api
    Signed-off-by: Jongmin Kim <whdalsrnt@megazone.com>

--- src/controllers/power-scheduler/schedule/schedule-resource-groups.js
+++ src/controllers/power-scheduler/schedule/schedule-resource-groups.js
@@ -205,7 +205,8 @@ const makeCreateResponseData = async (projectId, includeResourceGroup) => {
                             }],
                             'options': {},
                             'tags': {}
-                        }
+                        },
+                        'recommended': true
                     };
 
                     if (resourceType.startsWith('inventory.Server')) {
