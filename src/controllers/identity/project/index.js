f53e6a0ced621b38b511d38b634d968afe69e5b9
Jongmin Kim <whdalsrnt@megazone.com>
2020-10-22 21:30:17 +0900

    Refactor: include service account to project info
    Signed-off-by: Jongmin Kim <whdalsrnt@megazone.com>

--- src/controllers/identity/project/index.js
+++ src/controllers/identity/project/index.js
@@ -1,4 +1,5 @@
 import grpcClient from '@lib/grpc-client';
+import { listServiceAccounts } from '@controllers/identity/service-account';
 import _ from 'lodash';
 
 const CASCADE_DELETE_RESOURCES = [
@@ -36,34 +37,14 @@ const getProject = async (params) => {
     let response = await identityV1.Project.get(params);
 
     if(params.include_provider) {
-        //const providers = [];
-        const service_params = {
+        const serviceAccountResponse = await listServiceAccounts({
             project_id: params.project_id,
             domain_id:params.domain_id
-        };
+        });
 
-        const service_accounts = await identityV1.ServiceAccount.list(service_params);
-
-        if(service_accounts.total_count > 0){
-            const prep = _.uniqBy(service_accounts.results, 'provider').map(a => a.provider);
-            /*for (let i=0; i < prep.length; i++) {
-                const provider = prep[i];
-                try {
-                    const providerPram = { provider }
-                    if (providerPram.domain_id) {
-                        providerPram.domain_id = params.domain_id;
-                    }
-
-                    const providerOutput = await identityV1.Provider.get(providerPram);
-                    const providerName = _.get(providerOutput, 'name');
-                    providers.push(providerName);
-
-                } catch (e) {
-                    console.log(e);
-                }
-            }*/
-            response.providers = prep.length > 0 ? prep : [];
-        }
+        response.providers = serviceAccountResponse.results.map((serviceAccountInfo) => {
+            return serviceAccountInfo.provider;
+        });
     }
 
     return response;
@@ -165,22 +146,19 @@ const listProjects = async (params) => {
     let identityV1 = await grpcClient.get('identity', 'v1');
     let response = await identityV1.Project.list(params);
 
-    if (params.include_provider && response.results.length > 0) {
-        for(let i =0 ; i < response.results.length; i++ ){
-            const currentProject = response.results[i];
-            const service_params = {
-                project_id: currentProject.project_id,
+    if (params.include_provider) {
+        response.results = await Promise.all(response.results.map(async (projectInfo) => {
+            const serviceAccountResponse = await listServiceAccounts({
+                project_id: projectInfo.project_id,
                 domain_id:params.domain_id
-            };
-            const service_accounts = await identityV1.ServiceAccount.list(service_params);
-            if (service_accounts.total_count > 0) {
-                const prep = _.uniqBy(service_accounts.results, 'provider').map(a => a.provider);
-                response.results[i].providers = prep.length > 0 ? prep : [];
-            } else {
-                response.results[i].providers = [];
-            }
-        }
+            });
 
+            projectInfo['providers'] = serviceAccountResponse.results.map((serviceAccountInfo) => {
+                return serviceAccountInfo.provider;
+            });
+
+            return projectInfo;
+        }));
     }
 
     return response;
