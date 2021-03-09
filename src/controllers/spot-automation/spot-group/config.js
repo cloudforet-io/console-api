0e6c5a720b845a32c8951c1395e9f406b01f98c5
WANJIN <wanjin@megazone.com>
2021-03-09 15:31:30 +0900

    Add icon to spot group supported resource type item (#208)

--- src/controllers/spot-automation/spot-group/config.js
+++ src/controllers/spot-automation/spot-group/config.js
@@ -1,6 +1,7 @@
 export const SUPPORTED_RESOURCE_TYPES = {
     'inventory.CloudService?provider=aws&cloud_service_group=EC2&cloud_service_type=AutoScalingGroup': {
         'name': '[AWS] Auto Scaling Group',
-        'recommended_title': 'Auto Scaling Group'
+        'recommended_title': 'Auto Scaling Group',
+        icon: 'https://spaceone-custom-assets.s3.ap-northeast-2.amazonaws.com/console-assets/icons/cloud-services/aws/Amazon-EC2-Auto-Scaling.svg'
     }
 };
