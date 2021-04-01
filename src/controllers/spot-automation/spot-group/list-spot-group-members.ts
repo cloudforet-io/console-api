e438f4f9c3c854a304af49d38cd0aa96f52ce094
Jongmin Kim <whdalsrnt@megazone.com>
2021-04-01 17:10:03 +0900

    Add a listSpotGroupMembers API
    Signed-off-by: Jongmin Kim <whdalsrnt@megazone.com>

---
+++ src/controllers/spot-automation/spot-group/list-spot-group-members.ts
@@ -0,0 +1,22 @@
+import { getSpotGroup } from '@controllers/spot-automation/spot-group';
+import { listRoleBindings } from '@controllers/identity/role-binding';
+import logger from '@lib/logger';
+
+const listSpotGroupMembers = async (params) => {
+    if (!params.spot_group_id) {
+        throw new Error('Required Parameter. (key = spot_group_id)');
+    }
+
+    const spotGroupInfo = await getSpotGroup(params);
+    const query = params.query || {};
+    query.filter = query.filter || [];
+    query.filter.push({
+        k: 'project_id',
+        v: spotGroupInfo.project_id,
+        o: 'eq'
+    });
+    const response = await listRoleBindings({query: query});
+    return response;
+};
+
+export default listSpotGroupMembers;
