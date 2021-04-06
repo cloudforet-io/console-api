b87b45401f5c0cc6f24e46c9cdfa9a1597a49bc0
Jongmin Kim <whdalsrnt@megazone.com>
2021-04-06 11:59:45 +0900

    Refactor spot group APIs
    Signed-off-by: Jongmin Kim <whdalsrnt@megazone.com>

--- src/controllers/spot-automation/spot-group/get-spot-group-instance-count.ts
+++ src/controllers/spot-automation/spot-group/get-spot-group-instance-count.ts
@@ -1,82 +1,18 @@
 import { getValueByPath } from '@lib/utils';
-import { listSpotGroups } from '@controllers/spot-automation/spot-group';
-import { statCloudServices } from '@controllers/inventory/cloud-service';
+import { listSpotGroupResources } from './utils';
 
 
-const getSpotGroupResourceMap = async (spotGroupIds) => {
-    const requestParams = {
-        query: {
-            filter: [
-                {
-                    k: 'spot_group_id',
-                    v: spotGroupIds,
-                    o: 'in'
-                }
-            ],
-            only: ['spot_group_id', 'resource_id', 'resource_type']
-        }
-    };
-
-    const spotGroupsInfo = await listSpotGroups(requestParams);
-    const spotGroupMap = {};
-
-    spotGroupsInfo.results.forEach((spotGroupInfo) => {
-        spotGroupMap[spotGroupInfo.resource_id] = spotGroupInfo.spot_group_id;
-    });
-
-    return spotGroupMap;
-};
-
-const statAutoScalingGroupInstanceCount = async (spotGroupMap) => {
-    const requestParams = {
-        query: {
-            aggregate: [
-                {
-                    group: {
-                        keys: [
-                            {
-                                key: 'cloud_service_id',
-                                name: 'cloud_service_id'
-                            },
-                            {
-                                key: 'data.instances.lifecycle',
-                                name: 'lifecycle'
-                            }
-                        ]
-                    }
-                }
-            ],
-            filter: [
-                {
-                    k: 'cloud_service_id',
-                    v: Object.keys(spotGroupMap),
-                    o: 'in'
-                }
-            ]
-        }
-    };
-
-    return await statCloudServices(requestParams);
-};
-
-const makeResponse = (allSpotGroupIds, spotGroupMap, statResponse) => {
+const makeResponse = (spotGroupResources) => {
     const spotGroupsCount = {};
 
-    allSpotGroupIds.forEach((spotGroupId) => {
-        spotGroupsCount[spotGroupId] = {
-            total: 0,
-            spot: 0,
-            ondemand: 0
-        };
-    });
-
-    statResponse.results.forEach((data) => {
-        const spotGroupId = spotGroupMap[data.cloud_service_id];
+    Object.keys(spotGroupResources).forEach((spotGroupId) => {
+        const resourceInfo = spotGroupResources[spotGroupId];
+        const instanceLifecycles = getValueByPath(resourceInfo, 'data.instances.lifecycle');
         let total = 0;
         let ondemand = 0;
         let spot = 0;
 
-        data.lifecycle.forEach((lifecycle) => {
+        instanceLifecycles.forEach((lifecycle) => {
             if (lifecycle === 'scheduled') {
                 ondemand++;
             } else {
@@ -102,9 +38,8 @@ const getSpotGroupInstanceCount = async (params) => {
         throw new Error('Required Parameter. (key = spot_groups)');
     }
 
-    const spotGroupMap = await getSpotGroupResourceMap(params.spot_groups);
-    const statResponse = await statAutoScalingGroupInstanceCount(spotGroupMap);
-    return makeResponse(params.spot_groups, spotGroupMap, statResponse);
+    const spotGroupResources = await listSpotGroupResources(params.spot_groups);
+    return makeResponse(spotGroupResources);
 };
 
 
---
+++ src/controllers/spot-automation/spot-group/utils.ts
@@ -0,0 +1,89 @@
+import { getValueByPath } from '@lib/utils';
+import { listSpotGroups } from '@controllers/spot-automation/spot-group';
+import { listCloudServices } from '@controllers/inventory/cloud-service';
+import { listServers } from '@controllers/inventory/server';
+
+export const listSpotGroupResources = async (spotGroups) => {
+    const spotGroupsInfo = await listSpotGroups({
+        query: {
+            filter: [
+                {
+                    k: 'spot_group_id',
+                    v: spotGroups,
+                    o: 'in'
+                }
+            ]
+        },
+        only: ['spot_group_id', 'resource_id', 'resource_type']
+    });
+
+    const cloudServiceMap = {};
+
+    spotGroupsInfo.results.forEach((spotGroupInfo) => {
+        cloudServiceMap[spotGroupInfo.resource_id] = spotGroupInfo.spot_group_id;
+    });
+
+    const requestParams = {
+        query: {
+            filter: [
+                {
+                    k: 'cloud_service_id',
+                    v: Object.keys(cloudServiceMap),
+                    o: 'in'
+                }
+            ],
+            only: ['cloud_service_id', 'provider', 'cloud_service_group', 'cloud_service_type',
+                'data.instances.instance_id', 'data.instances.lifecycle']
+        }
+    };
+
+    const response = await listCloudServices(requestParams);
+    const spotGroupResourcesInfo = {};
+
+    response.results.forEach((cloudServiceInfo) => {
+        if (cloudServiceInfo['cloud_service_group'] === 'EKS' && cloudServiceInfo['cloud_service_type'] === 'NodeGroup') {
+            // TODO: get ASG from EKS
+        } else {
+            spotGroupResourcesInfo[cloudServiceMap[cloudServiceInfo['cloud_service_id']]] = cloudServiceInfo;
+        }
+    });
+
+    return spotGroupResourcesInfo;
+};
+
+export const listSpotGroupServers = async (spotGroups) => {
+    const spotGroupResourcesInfo = await listSpotGroupResources(spotGroups);
+    const spotGroupServers = {};
+
+    const promises = Object.keys(spotGroupResourcesInfo).map(async (spotGroupId) => {
+        const resourceInfo = spotGroupResourcesInfo[spotGroupId];
+        const resourceType = `${resourceInfo['provider']}.${resourceInfo['cloud_service_group']}.${resourceInfo['cloud_service_type']}`;
+
+        if (resourceType === 'aws.EC2.AutoScalingGroup') {
+            const instanceIds = getValueByPath(resourceInfo, 'data.instances.instance_id');
+            const requestParams = {
+                query: {
+                    filter: [
+                        {
+                            k: 'reference.resource_id',
+                            v: instanceIds,
+                            o: 'in'
+                        }
+                    ],
+                    only: ['server_id', 'name']
+                }
+            };
+            const response = await listServers(requestParams);
+            spotGroupServers[spotGroupId] = response.results.map((serverInfo) => {
+                return {
+                    server_id: serverInfo.server_id,
+                    name: serverInfo.name
+                };
+            });
+        }
+    });
+
+    await Promise.all(promises);
+
+    return spotGroupServers;
+};
