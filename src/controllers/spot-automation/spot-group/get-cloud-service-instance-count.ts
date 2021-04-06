fae3efe7c5da3a0b7fc10a61977bef93a808fd29
Jongmin Kim <whdalsrnt@megazone.com>
2021-04-06 13:51:31 +0900

    Fix a EKS Node Group compatibility
    Signed-off-by: Jongmin Kim <whdalsrnt@megazone.com>

--- src/controllers/spot-automation/spot-group/get-cloud-service-instance-count.ts
+++ src/controllers/spot-automation/spot-group/get-cloud-service-instance-count.ts
@@ -1,10 +1,26 @@
 import { getValueByPath } from '@lib/utils';
-import { getCloudService } from '@controllers/inventory/cloud-service';
+import { getCloudService, listCloudServices } from '@controllers/inventory/cloud-service';
 
 const getCloudServiceInfo = async (cloudServiceId) => {
     const cloudServiceInfo = await getCloudService({cloud_service_id: cloudServiceId});
     if (cloudServiceInfo['cloud_service_group'] === 'EKS' && cloudServiceInfo['cloud_service_type'] === 'NodeGroup') {
-        // TODO: get ASG from EKS
+        const autoScalingGroupsARN = getValueByPath(cloudServiceInfo, 'data.resources.auto_scaling_groups.arn');
+        const requestParams = {
+            query: {
+                filter: [
+                    {
+                        k: 'reference.resource_id',
+                        v: autoScalingGroupsARN,
+                        o: 'in'
+                    }
+                ],
+                only: ['data.desired_capacity']
+            }
+        };
+        const response = await listCloudServices(requestParams);
+        if (response.total_count > 0) {
+            return response.results[0];
+        }
     } else {
         return cloudServiceInfo;
     }
@@ -18,9 +34,8 @@ const getCloudServiceInstanceCount = async (params) => {
     const cloudServiceInfo = await getCloudServiceInfo(params.cloud_service_id);
     const instanceCount = getValueByPath(cloudServiceInfo, 'data.desired_capacity');
     return {
-        count: instanceCount
+        count: instanceCount || 0
     };
 };
-
 
 export default getCloudServiceInstanceCount;
