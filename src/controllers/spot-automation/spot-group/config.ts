b386a65f98a40f6d0644abd477ea6d4a0182aaac
WANJIN <wanjin@megazone.com>
2021-04-07 18:30:41 +0900

    Remove eks node group from spot group supported resource type (#235)

--- src/controllers/spot-automation/spot-group/config.ts
+++ src/controllers/spot-automation/spot-group/config.ts
@@ -3,12 +3,12 @@ export const SUPPORTED_RESOURCE_TYPES = {
         'name': '[AWS] Auto Scaling Group',
         'recommended_title': 'Auto Scaling Group',
         icon: 'https://spaceone-custom-assets.s3.ap-northeast-2.amazonaws.com/console-assets/icons/cloud-services/aws/Amazon-EC2-Auto-Scaling.svg'
-    },
-    'inventory.CloudService?provider=aws&cloud_service_group=EKS&cloud_service_type=Cluster': {
-        'name': '[AWS] EKS Node Group',
-        'recommended_title': 'EKS Node Group',
-        icon: 'https://spaceone-custom-assets.s3.ap-northeast-2.amazonaws.com/console-assets/icons/cloud-services/aws/Amazon-Elastic-Kubernetes-Service.svg'
     }
+    // 'inventory.CloudService?provider=aws&cloud_service_group=EKS&cloud_service_type=Cluster': {
+    //     'name': '[AWS] EKS Node Group',
+    //     'recommended_title': 'EKS Node Group',
+    //     icon: 'https://spaceone-custom-assets.s3.ap-northeast-2.amazonaws.com/console-assets/icons/cloud-services/aws/Amazon-Elastic-Kubernetes-Service.svg'
+    // }
 };
 
 export const SERVICE_TYPE = {
