b44c3993c242d86f2c5997a6c885ed0bebc9b3ed
Jongmin Kim <whdalsrnt@megazone.com>
2021-04-09 21:13:29 +0900

    Refactor get-spot-group-instance-count API
    Signed-off-by: Jongmin Kim <whdalsrnt@megazone.com>

--- src/controllers/spot-automation/spot-group/get-spot-group-instance-count.ts
+++ src/controllers/spot-automation/spot-group/get-spot-group-instance-count.ts
@@ -1,46 +1,102 @@
-import { getValueByPath } from '@lib/utils';
-import { listSpotGroupResources } from './utils';
+import { statHistory } from '@controllers/spot-automation/history';
 
-
-const makeResponse = (spotGroupResources) => {
-    const spotGroupsCount = {};
-
-    Object.keys(spotGroupResources).forEach((spotGroupId) => {
-        const resourceInfo = spotGroupResources[spotGroupId];
-        const instanceLifecycles = getValueByPath(resourceInfo, 'data.instances.lifecycle');
-        let total = 0;
-        let ondemand = 0;
-        let spot = 0;
-
-        instanceLifecycles.forEach((lifecycle) => {
-            if (lifecycle === 'scheduled') {
-                ondemand++;
-            } else {
-                spot++;
-            }
-            total++;
-        });
-
-        spotGroupsCount[spotGroupId] = {
-            total,
-            ondemand,
-            spot
-        };
-    });
-
+const getDefaultQuery = () => {
     return {
-        spot_groups: spotGroupsCount
+        query: {
+            aggregate: [
+                {
+                    sort: {
+                        key: 'created_at'
+                    }
+                },
+                {
+                    group: {
+                        keys: [
+                            {
+                                key: 'spot_group_id',
+                                name: 'spot_group_id'
+                            }
+                        ],
+                        fields: [
+                            {
+                                name: 'total',
+                                key: 'total_count',
+                                operator: 'last'
+                            },
+                            {
+                                name: 'spot',
+                                key: 'spot_count',
+                                operator: 'last'
+                            },
+                            {
+                                name: 'ondemand',
+                                key: 'ondemand_count',
+                                operator: 'last'
+                            }
+                        ]
+                    }
+                }
+            ],
+            filter: [
+                {
+                    k: 'created_at',
+                    v: 'now/d-1d',
+                    o: 'timediff_gte'
+                }
+            ]
+        }
     };
 };
 
-const getSpotGroupInstanceCount = async (params) => {
+const makeRequest = (params) => {
     if (!params.spot_groups) {
         throw new Error('Required Parameter. (key = spot_groups)');
     }
 
-    const spotGroupResources = await listSpotGroupResources(params.spot_groups, ['data.instances']);
-    return makeResponse(spotGroupResources);
+    const requestParams = getDefaultQuery();
+
+    requestParams['query']['filter'].push({
+        k: 'spot_group_id',
+        v: params.spot_groups,
+        o: 'in'
+    });
+
+    return requestParams;
 };
 
+const makeResponse = (results, spotGroups) => {
+    const spotGroupResults = {};
+    spotGroups.forEach((spotGroupId) => {
+        // spotGroupResults[spotGroupId] = {
+        //     total: 0,
+        //     spot: 0,
+        //     ondemand: 0
+        // };
+        const total = Math.floor(Math.random() * 10);
+        spotGroupResults[spotGroupId] = {
+            total: total,
+            spot: total,
+            ondemand: 0
+        };
+    });
+
+    results.forEach((item) => {
+        spotGroupResults[item.spot_group_id] = {
+            total: item.total || 0,
+            spot: item.spot || 0,
+            ondemand: item.ondemand || 0
+        };
+    });
+
+    return {
+        spot_groups: spotGroupResults
+    };
+};
+
+const getSpotGroupInstanceCount = async (params) => {
+    const requestParams = makeRequest(params);
+    const response = await statHistory(requestParams);
+    return makeResponse(response.results, params.spot_groups);
+};
 
 export default getSpotGroupInstanceCount;
