3908337813868c0b40261a103d8aeed35984e55b
Jongmin Kim <whdalsrnt@megazone.com>
2022-04-29 11:25:44 +0900

    feat: add a resource-by-region stat API (#457)

---
+++ src/controllers/statistics/topic/resource-by-region.ts
@@ -0,0 +1,146 @@
+import _ from 'lodash';
+import { statCloudServices } from '@controllers/inventory/cloud-service';
+import { requestCache } from './request-cache';
+
+const getDefaultQuery = () => {
+    return {
+        query: {
+            aggregate: [{
+                group: {
+                    keys: [
+                        {
+                            key: 'provider',
+                            name: 'provider'
+                        },
+                        {
+                            key: 'region_code',
+                            name: 'region_code'
+                        },
+                        {
+                            key: 'cloud_service_group',
+                            name: 'cloud_service_group'
+                        },
+                        {
+                            key: 'cloud_service_type',
+                            name: 'cloud_service_type'
+                        }
+                    ],
+                    fields: [
+                        {
+                            name: 'count',
+                            operator: 'count'
+                        }
+                    ]
+                }
+            }, {
+                group: {
+                    keys: [
+                        {
+                            key: 'provider',
+                            name: 'provider'
+                        },
+                        {
+                            key: 'region_code',
+                            name: 'region_code'
+                        }
+                    ],
+                    fields: [
+                        {
+                            name: 'total_count',
+                            key: 'count',
+                            operator: 'sum'
+                        },
+                        {
+                            name: 'cloud_services',
+                            operator: 'push',
+                            fields: [
+                                {
+                                    key: 'cloud_service_group',
+                                    name: 'cloud_service_group'
+                                },
+                                {
+                                    key: 'cloud_service_type',
+                                    name: 'cloud_service_type'
+                                },
+                                {
+                                    key: 'count',
+                                    name: 'count'
+                                }
+                            ]
+                        }
+                    ]
+                }
+            }, {
+                sort: {
+                    key: 'total_count',
+                    desc: true
+
+                }
+            }],
+            filter: [
+                {
+                    k: 'ref_cloud_service_type.is_major',
+                    v: true,
+                    o: 'eq'
+                },
+                {
+                    k: 'region_code',
+                    v: null,
+                    o: 'not'
+                }
+            ]
+        }
+    };
+};
+
+const makeRequest = (params) => {
+    const requestParams = getDefaultQuery();
+
+    if (params.project_id) {
+        requestParams.query.filter.push({
+            k: 'project_id',
+            v: params.project_id,
+            o: 'eq'
+        });
+    }
+
+    if (params.providers) {
+        if (!Array.isArray(params.providers)) {
+            throw new Error('Parameter type is invalid. (providers = Array)');
+        }
+
+        requestParams.query.filter.push({
+            k: 'provider',
+            v: params.providers,
+            o: 'in'
+        });
+    }
+
+    if (params.domain_id) {
+        requestParams['domain_id'] = params.domain_id;
+    }
+
+    return requestParams;
+};
+
+
+const makeResponse = (response) => {
+    for (const result of response.results) {
+        result.cloud_services = _.sortBy(result.cloud_services, 'count').reverse();
+    }
+
+    return response;
+};
+
+const requestStat = async (params) => {
+    const requestParams = makeRequest(params);
+    const response = await statCloudServices(requestParams);
+
+    return makeResponse(response);
+};
+
+const resourceByRegion = async (params) => {
+    return await requestCache('stat:resourceByRegion', params, requestStat);
+};
+
+export default resourceByRegion;
