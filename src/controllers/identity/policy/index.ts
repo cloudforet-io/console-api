b31a8af106542e5770d92a13b6e13c80887dcb0c
Minseo Lee <mmmm@ajou.ac.kr>
2022-05-09 16:03:07 +0900

    feat: merge identity & repository polish (#461)
    * feat: merge identity & repository polish
    
    * feat: merge identity & repository polish
    
    remove annotations
    
    * feat: merge identity & repository polish
    
    improve some readability
    
    * feat: merge identity & repository polish
    
    improve readablitiy

--- src/controllers/identity/policy/index.ts
+++ src/controllers/identity/policy/index.ts
@@ -30,9 +30,27 @@ const getPolicy = async (params) => {
 
 const listPolicies = async (params) => {
     const identityV1 = await grpcClient.get('identity', 'v1');
-    const response = await identityV1.Policy.list(params);
+    const identityResponse = await identityV1.Policy.list(params);
 
-    return response;
+    /*1. Collect all repository_id
+    2. Collect repository.Policy with the set of repository_id
+    3. Merge repository.Policy into identity.Policy*/
+    const repositoryV1 = await grpcClient.get('repository', 'v1');
+    const repositories = await repositoryV1.Repository.list(params);
+    const repositoryPromise = repositories.results.map(d => (repositoryV1.Policy.list({ ...params, repository_id: d.repository_id })));
+    const repositoryResponse = await Promise.allSettled(repositoryPromise);
+
+    const policies = [...identityResponse.results];
+    repositoryResponse.forEach(d => {
+        if (d.status === 'fulfilled') {
+            d.value.results.forEach(policy => policies.push(policy));
+        }
+    });
+
+    return {
+        results: policies,
+        total_count: policies.length
+    };
 };
 
 const statPolicies = async (params) => {
