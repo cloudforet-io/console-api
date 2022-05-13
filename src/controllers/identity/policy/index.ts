37703039673984b59d07ccce5fd4ae2e198f2d97
Dahyun Yu <aydha0110@gmail.com>
2022-05-13 13:31:24 +0900

    fix: add policy_type field to policy list response (#463)
    * fix: add policy_type field to policy list response
    
    * fix: set totalCount to listRepositoryPolicy
    
    * chore: edit minor

--- src/controllers/identity/policy/index.ts
+++ src/controllers/identity/policy/index.ts
@@ -28,28 +28,51 @@ const getPolicy = async (params) => {
     return response;
 };
 
-const listPolicies = async (params) => {
+
+const listIdentityPolicy = async (params) => {
     const identityV1 = await grpcClient.get('identity', 'v1');
-    const identityResponse = await identityV1.Policy.list(params);
-
-    /*1. Collect all repository_id
-    2. Collect repository.Policy with the set of repository_id
-    3. Merge repository.Policy into identity.Policy*/
+    const { results, total_count } = await identityV1.Policy.list(params);
+    const _results = results.map((d) => ({
+        ...d,
+        policy_type: 'CUSTOM'
+    }));
+    return { results: _results, total_count };
+};
+const listRepositoryPolicy = async (params) => {
+    /**
+     * 1. Collect all repository_id
+     * 2. Collect repository.Policy with the set of repository_id
+     * 3. Merge repository.Policy into identity.Policy
+    */
     const repositoryV1 = await grpcClient.get('repository', 'v1');
-    const repositories = await repositoryV1.Repository.list(params);
-    const repositoryPromise = repositories.results.map(d => (repositoryV1.Policy.list({ ...params, repository_id: d.repository_id })));
-    const repositoryResponse = await Promise.allSettled(repositoryPromise);
-
-    const policies = [...identityResponse.results];
-    repositoryResponse.forEach(d => {
-        if (d.status === 'fulfilled') {
-            d.value.results.forEach(policy => policies.push(policy));
+    const { results: repositoryResults } = await repositoryV1.Repository.list(params);
+    const repositoryResponse = await Promise.allSettled(repositoryResults.map(d => (
+        repositoryV1.Policy.list({ ...params, repository_id: d.repository_id })
+    )));
+    let results = [];
+    let totalCount = 0;
+    repositoryResponse.forEach((res) => {
+        if (res.status === 'fulfilled') {
+            const _totalCount = res.value.total_count;
+            const _results = res.value.results.map((r) => ({
+                ...r,
+                policy_type: 'MANAGED'
+            }));
+            results = results.concat(_results);
+            totalCount += _totalCount;
         }
     });
-
+    return { results, total_count: totalCount };
+};
+const listPolicies = async (params) => {
+    const [identityResponse, repositoryResponse] = await Promise.all([
+        listIdentityPolicy(params),
+        listRepositoryPolicy(params)
+    ]);
+    const policyResults = repositoryResponse.results.concat(identityResponse.results);
     return {
-        results: policies,
-        total_count: policies.length
+        results: policyResults,
+        total_count: identityResponse.total_count + repositoryResponse.total_count
     };
 };
 
