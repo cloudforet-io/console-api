88f191cee6db8a3fe8fa99ea1619a93706ce6369
Dahyun Yu <yuda@mz.co.kr>
2022-11-09 21:58:06 +0900

    feat(favorite): add `order-list/get` and `order-list/set` api
    * fix(favorite/recent): add `DASHBOARD` type to favorite, recent
    
    * feat(favorite): add `order-list/get` and `order-list/set` api

--- src/routes/add-ons/favorite.ts
+++ src/routes/add-ons/favorite.ts
@@ -2,13 +2,16 @@ import express from 'express';
 import asyncHandler from 'express-async-handler';
 
 import * as favorite from '@controllers/add-ons/favorite';
+import * as favoriteOrderList from '@controllers/add-ons/favorite/order-list';
 
 const router = express.Router();
 
 const controllers = [
     { url: '/create', func: favorite.createFavorite },
     { url: '/list', func: favorite.listFavorites },
-    { url: '/delete', func: favorite.deleteFavorites }
+    { url: '/delete', func: favorite.deleteFavorites },
+    { url: '/order-list/set', func: favoriteOrderList.setFavoritesOrderList },
+    { url: '/order-list/get', func: favoriteOrderList.getFavoritesOrderList }
 ];
 
 controllers.forEach((config) => {
