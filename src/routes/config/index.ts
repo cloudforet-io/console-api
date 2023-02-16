1c9cd567a0d34df32b46ba27363599d171cab281
Jongmin Kim <whdalsrnt@megazone.com>
2023-02-16 16:30:28 +0900

    feat: add config.DomainConfig APIs
    (cherry picked from commit 0f528fa41fd2ef8046e235f1217aa88c31928447)

--- src/routes/config/index.ts
+++ src/routes/config/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 
+import domainConfigRouter from './domain-config';
 import userConfigRouter from './user-config';
 
 const router = express.Router();
 
 router.use('/user-config', userConfigRouter);
+router.use('/domain-config', domainConfigRouter);
 
 export default router;
